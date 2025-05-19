32f8841904e4de989bcf240ebe22eb2c3ff06716
Eunjun1 <ibbuni8486@gmail.com>
2025-05-19 11:58:03 +0900

    orderying insert 추가

--- python_FASTAPI/orderying.py
+++ python_FASTAPI/orderying.py
@@ -15,6 +15,22 @@ def connect():
 
 
 
+
+
+@router.get("/")
+async def selectAll():
+
+    conn = connect()
+    curs = conn.cursor()
+
+    curs.execute("select * from orderying")
+    rows = curs.fetchall()
+    conn.close()
+
+    result = [{"ody_num":row[0],"emp_code":row[1],"doc_code":row[2],"prod_code":row[3],"ody_type":row[4],"ody_date":row[5],"ody_count":row[6],"reject_reason":row[7]} for row in rows]
+    return {'results':result}
+
+
 @router.get("/{ody_num}")
 async def selectAll(ody_num : int):
 
@@ -27,3 +43,18 @@ async def selectAll(ody_num : int):
 
     result = [{"ody_num":row[0],"emp_code":row[1],"doc_code":row[2],"prod_code":row[3],"ody_type":row[4],"ody_date":row[5],"ody_count":row[6],"reject_reason":row[7]} for row in rows]
     return {'results':result}
+
+
+@router.post("/insert")
+async def insert(emp_code:int=Form(...),doc_code:int=Form(...),prod_code:int=Form(...),ody_type:int=Form(...),ody_date:str=Form(...),ody_count:int=Form(...),reject_reason:str=Form(...)):
+    try:
+        conn = connect()
+        curs = conn.cursor()
+        sql = 'insert into orderying values (emp_code,doc_code,prod_code,ody_type,ody_date,ody_count,reject_reason)'
+        curs.execute(sql,(emp_code,doc_code,prod_code,ody_type,ody_date,ody_count,reject_reason))
+        conn.commit()
+        conn.close()
+        return {"result":"OK"}
+    except Exception as e:
+        print("Error : ",e)
+        return {"result":"Error"}
