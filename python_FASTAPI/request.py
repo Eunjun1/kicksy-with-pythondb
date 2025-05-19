537b781dabb1d64ffb50c65b272619583a7c3f31
wodnjsrla85 <wodnjsrla85@naver.com>
2025-05-19 11:44:17 +0900

    request insert

--- python_FASTAPI/request.py
+++ python_FASTAPI/request.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Form
+from pydantic import BaseModel
 import pymysql
 
 
@@ -13,17 +14,39 @@ def connect():
         charset="utf8",
     )
 
+class request(BaseModel) :
+    user_email : str
+    product_prod_code : int
+    store_str_code : int
+    req_type : int
+    req_count : int
+    reason : str
 
-
-@router.get("/{req_num}")
-async def selectAll(req_num : int):
+@router.get("/{user_email}")
+async def selectAll(user_email : str):
 
     conn = connect()
     curs = conn.cursor()
 
-    curs.execute("select * from request where req_num = %s",(req_num,))
+    curs.execute("select * from request where user_email = %s",(user_email,))
     rows = curs.fetchall()
     conn.close()
 
     result = [{"req_num":row[0],"user_email":row[1],"product_prod_code":row[2],"store_str_code":row[3],"req_type":row[4],"req_date":row[5],"req_count":row[6],"reason":row[7]} for row in rows]
     return {'results':result}
+
+@router.post("/insert")
+async def insert(request : request):
+    conn = connect()
+    curs = conn.cursor()
+
+    try : 
+        sql='insert into request(user_email,product_prod_code,store_str_code,req_type,req_date,req_count,reason) values (%s,%s,%s,%s,now(),%s,%s)'
+        curs.execute(sql,(request.user_email,request.product_prod_code,request.store_str_code,request.req_type,request.req_count,request.reason))
+        conn.commit()
+        conn.close()
+        return {'result' : 'OK'}
+    except Exception as ex :
+        conn.close()
+        print("Error : ", ex)
+        return {'result' : 'Error'}
